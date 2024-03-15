6a28ba56618c8af043529cf1c697a7b6539a94c5
wdelcol12 <wdelcol@gmail.com>
2024-03-15 14:33:49 -0400

    03/12 activities

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,21 @@
 const express = require('express')
 const router = express.Router();
+
+router.get('usernames', (req,res) => {
+    let usernames = req.users.map(function(user) {
+        return {id: user.id, username: user.username};
+    });
+    res.send(usernames);
+})
+
+router.get('/usernames/:name', (req,res) => {
+    let name = req.params.name
+    req.users.map(function(user) {
+        if (user.username === name) {
+          username = user.username
+        }
+      });
+      res.send(username);
+})
+
+module.exports = router;
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -2,3 +2,15 @@ const express = require('express')
 const fs = require('fs');
 const path = require('path');
 const router = express.Router()
+
+
+router.post('/adduser', (req,res) => {
+    let newuser = req.body;
+    req.users.push(newuser);
+    fs.writeFile(path.resolve(__dirname, '../data/users.json'))
+    console.log('User saved')
+
+    res.send("User added")
+})
+
+module.exports = router;
